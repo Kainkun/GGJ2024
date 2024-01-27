41219294e437565bcf2eb9bd02a818d5fe06e0e9
Kain Shepard <kainkun100@gmail.com>
2024-01-27 12:57:33 -0800

    game intro outro flow

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void StartMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void StartEpilogue()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            GetComponent<Animator>().SetTrigger("FadeOut");
+        }
+    }
+}
