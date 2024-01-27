6ea27c8b199242d3b5fb8360d12ea8d09c2c4132
Kain Shepard <kainkun100@gmail.com>
2024-01-26 23:27:43 -0800

    fade

--- Assets/Scripts/Fade.cs
+++ Assets/Scripts/Fade.cs
@@ -8,37 +8,79 @@ using UnityEngine.UI;
 public class Fade : MonoBehaviour
 {
     public Image black;
-    public AnimationCurve curve;
+    public AnimationCurve fadeToGame;
+    public AnimationCurve fadeToEpilogue;
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
     }
 
-    public void FadeToScene(int scene)
+    public void FadeToGame()
     {
-        StartCoroutine(CR_FadeToScene(scene));
+        StartCoroutine(CR_FadeToGame());
     }
 
-    IEnumerator CR_FadeToScene(int scene)
+    IEnumerator CR_FadeToGame()
     {
         float t = 0;
-        while (t < curve.keys[curve.length - 1].time)
+        float value;
+        while (t < fadeToGame.keys[fadeToGame.length - 1].time)
         {
             t += Time.deltaTime;
-            float a = curve.Evaluate(t);
-            black.color = new Color(0, 0, 0, a);
+            value = fadeToGame.Evaluate(t);
+            black.color = new Color(0, 0, 0, value);
             yield return null;
         }
 
-        SceneManager.LoadScene(scene);
+        black.color = new Color(0, 0, 0, 1);
+
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(1);
+        yield return new WaitForSeconds(0.5f);
 
         while (t > 0)
         {
             t -= Time.deltaTime;
-            float a = curve.Evaluate(t);
-            black.color = new Color(0, 0, 0, a);
+            value = fadeToGame.Evaluate(t);
+            black.color = new Color(0, 0, 0, value);
             yield return null;
         }
+
+        black.color = new Color(0, 0, 0, 0);
+    }
+
+    public void FadeToEpilogue()
+    {
+        StartCoroutine(CR_FadeToEpilogue());
+    }
+
+    IEnumerator CR_FadeToEpilogue()
+    {
+        float t = 0;
+        float value;
+        while (t < fadeToEpilogue.keys[fadeToEpilogue.length - 1].time)
+        {
+            t += Time.deltaTime;
+            value = fadeToEpilogue.Evaluate(t);
+            black.color = new Color(0, 0, 0, value);
+            yield return null;
+        }
+
+        black.color = new Color(0, 0, 0, 1);
+
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(2);
+        yield return new WaitForSeconds(0.5f);
+
+        while (t > 0)
+        {
+            t -= Time.deltaTime;
+            value = fadeToEpilogue.Evaluate(t);
+            black.color = new Color(0, 0, 0, value);
+            yield return null;
+        }
+
+        black.color = new Color(0, 0, 0, 0);
     }
 }
