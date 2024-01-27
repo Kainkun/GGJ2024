d540d58ac088ecbd34c497eb821455a9490bc918
Kain Shepard <kainkun100@gmail.com>
2024-01-26 23:14:38 -0800

    fade

--- Assets/Scripts/Fade.cs
+++ Assets/Scripts/Fade.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Fade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image black;
+    public AnimationCurve curve;
+
+    private void Awake()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FadeToScene(int scene)
     {
-        
+        StartCoroutine(CR_FadeToScene(scene));
+    }
+
+    IEnumerator CR_FadeToScene(int scene)
+    {
+        float t = 0;
+        while (t < curve.keys[curve.length - 1].time)
+        {
+            t += Time.deltaTime;
+            float a = curve.Evaluate(t);
+            black.color = new Color(0, 0, 0, a);
+            yield return null;
+        }
+
+        SceneManager.LoadScene(scene);
+
+        while (t > 0)
+        {
+            t -= Time.deltaTime;
+            float a = curve.Evaluate(t);
+            black.color = new Color(0, 0, 0, a);
+            yield return null;
+        }
     }
 }
