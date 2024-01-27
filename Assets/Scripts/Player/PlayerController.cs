033af40695bed146a2e46dde0a568cad040dd14e
Daniel Walls <walls.dk@gmail.com>
2024-01-27 12:28:46 -0800

    finished player statemachine setup

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,44 +1,36 @@
 ﻿using UnityEngine;
 
+public abstract class PlayerState : State
+{
+    protected PlayerController Controller { get; private set; }
+    
+    public void Initialize(PlayerController controller)
+    {
+        Controller = controller;
+        OnInitialize();
+    }
+    
+    public virtual void OnUpdate() {}
+    protected virtual void OnInitialize() {}
+}
+
 public class PlayerController : MonoBehaviour
 {
-    [Header("References")]
-    
-    [SerializeField]
-    private Transform yawTransform;
-    
-    [SerializeField]
-    private Transform pitchTransform;
+    public PlayerFreeMovementState freeMovementState;
 
-    [SerializeField] 
-    private CharacterController characterController;
-    
-    [Header("Settings")]
-        
-    [SerializeField]
-    private PlayerMovementSettings movementSettings;
-    
-    [SerializeField] 
-    private PlayerRotationSettings rotationSettings;
-        
-    private PlayerMovementLogic _movementLogic;
-    private PlayerRotationLogic _rotationLogic;
-    private PlayerInteractionLogic _interactionLogic;
+    public StateMachine<PlayerState> StateMachine { get; private set; }
 
     private void Awake()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        StateMachine = new StateMachine<PlayerState>();
         
-        _movementLogic = new PlayerMovementLogic(movementSettings, new PlayerMovementReferences{yawTransform = yawTransform, characterController = characterController});
-        _rotationLogic = new PlayerRotationLogic(rotationSettings, new PlayerRotationReferences{yawTransform = yawTransform, pitchTransform = pitchTransform});
-        _interactionLogic = new PlayerInteractionLogic(new PlayerInteractReferences{interactSource = gameObject, viewTransform = pitchTransform});
+        freeMovementState.Initialize(this);
+        
+        StateMachine.TransitionTo(freeMovementState);
     }
 
     private void Update()
     {
-        _movementLogic.Update(Time.deltaTime, new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized);
-        _rotationLogic.Update(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-        _interactionLogic.Update(Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Mouse0));
+        StateMachine.CurrentState?.OnUpdate();
     }
 }
---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,17 @@
+﻿public abstract class State
+{
+    public virtual void OnEnter() {}
+    public virtual void OnExit() {}
+}
+
+public class StateMachine<T> where T : State
+{
+    public T CurrentState { get; private set; }
+    
+    public void TransitionTo(T state)
+    {
+        CurrentState?.OnExit();
+        CurrentState = state;
+        CurrentState?.OnEnter();
+    }
+}
