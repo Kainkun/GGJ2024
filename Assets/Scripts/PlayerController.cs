83d64b98d1c4541df1305e20bbd2fc0f36bef6cc
Daniel Walls <walls.dk@gmail.com>
2024-01-26 22:40:16 -0800

    player control and prefab

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,24 @@
-﻿namespace DefaultNamespace
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
 {
-    public class PlayerController
-    {
+    [SerializeField]
+    private FpsPlayerReferences fpsReferences;
         
+    [SerializeField]
+    private FpsPlayerSettings fpsSettings;
+        
+    private FpsPlayerLogic _fpsLogic;
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        _fpsLogic = new FpsPlayerLogic(fpsSettings, fpsReferences);
+    }
+
+    private void Update()
+    {
+        _fpsLogic.Update(Time.deltaTime);
     }
 }
