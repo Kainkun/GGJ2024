20557f6ea3be2dbef2a31f1bb6b326c7d06d37e7
Kain Shepard <kainkun100@gmail.com>
2024-01-26 23:38:40 -0800

    fade

--- Assets/Scripts/Fade.cs
+++ Assets/Scripts/Fade.cs
@@ -7,80 +7,16 @@ using UnityEngine.UI;
 
 public class Fade : MonoBehaviour
 {
-    public Image black;
-    public AnimationCurve fadeToGame;
-    public AnimationCurve fadeToEpilogue;
+    Animator animator;
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
+        animator = GetComponent<Animator>();
     }
 
-    public void FadeToGame()
+    public void LoadGame()
     {
-        StartCoroutine(CR_FadeToGame());
-    }
-
-    IEnumerator CR_FadeToGame()
-    {
-        float t = 0;
-        float value;
-        while (t < fadeToGame.keys[fadeToGame.length - 1].time)
-        {
-            t += Time.deltaTime;
-            value = fadeToGame.Evaluate(t);
-            black.color = new Color(0, 0, 0, value);
-            yield return null;
-        }
-
-        black.color = new Color(0, 0, 0, 1);
-
-        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene(1);
-        yield return new WaitForSeconds(0.5f);
-
-        while (t > 0)
-        {
-            t -= Time.deltaTime;
-            value = fadeToGame.Evaluate(t);
-            black.color = new Color(0, 0, 0, value);
-            yield return null;
-        }
-
-        black.color = new Color(0, 0, 0, 0);
-    }
-
-    public void FadeToEpilogue()
-    {
-        StartCoroutine(CR_FadeToEpilogue());
-    }
-
-    IEnumerator CR_FadeToEpilogue()
-    {
-        float t = 0;
-        float value;
-        while (t < fadeToEpilogue.keys[fadeToEpilogue.length - 1].time)
-        {
-            t += Time.deltaTime;
-            value = fadeToEpilogue.Evaluate(t);
-            black.color = new Color(0, 0, 0, value);
-            yield return null;
-        }
-
-        black.color = new Color(0, 0, 0, 1);
-
-        yield return new WaitForSeconds(0.5f);
-        SceneManager.LoadScene(2);
-        yield return new WaitForSeconds(0.5f);
-
-        while (t > 0)
-        {
-            t -= Time.deltaTime;
-            value = fadeToEpilogue.Evaluate(t);
-            black.color = new Color(0, 0, 0, value);
-            yield return null;
-        }
-
-        black.color = new Color(0, 0, 0, 0);
     }
 }
