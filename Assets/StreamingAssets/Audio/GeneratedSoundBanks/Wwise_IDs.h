3b8baacab65977977626f525615695401e1c16a0
TheLittleCrow <Brendan.Julian@gmail.com>
2024-01-27 11:36:39 -0800

    Test Soundbanks for Wwise
    Added test dialogue

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,20 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_FEMALE_VOICE_GOODBYE = 3876234301U;
+        static const AkUniqueID PLAY_FEMALE_VOICE_HAVEANICEDAY = 738920550U;
+        static const AkUniqueID PLAY_FEMALE_VOICE_HELLO = 37077532U;
+        static const AkUniqueID PLAY_SEQUENCE_WELCOME_WHATCANIDO = 4123174421U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK = 1661994096U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
