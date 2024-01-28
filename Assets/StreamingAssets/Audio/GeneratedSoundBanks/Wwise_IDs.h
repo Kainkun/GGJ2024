bf890b307887d5e2374c19d1075080ea3df93f13
TheLittleCrow <Brendan.Julian@gmail.com>
2024-01-27 17:34:42 -0800

    Wwise
    Music Tweak

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -16,13 +16,14 @@ namespace AK
         static const AkUniqueID PLAY_FEMALE_VOICE_GOODBYE = 3876234301U;
         static const AkUniqueID PLAY_FEMALE_VOICE_HAVEANICEDAY = 738920550U;
         static const AkUniqueID PLAY_FEMALE_VOICE_HELLO = 37077532U;
+        static const AkUniqueID PLAY_MUZAK_GUITAR = 2931435471U;
         static const AkUniqueID PLAY_SEQUENCE_WELCOME_WHATCANIDO = 4123174421U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID DEFAULT = 782826392U;
+        static const AkUniqueID SOUNDBANK = 1661994096U;
     } // namespace BANKS
 
     namespace BUSSES
