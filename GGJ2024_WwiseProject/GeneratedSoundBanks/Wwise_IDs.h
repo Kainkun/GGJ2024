22a022636c2ef8d936c813fc3472ed423d1cb982
TheLittleCrow <Brendan.Julian@gmail.com>
2024-01-27 23:34:14 -0800

    Wwise
    PA Man Events

---
+++ GGJ2024_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,61 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_CASHIER_COUPON = 4243000246U;
+        static const AkUniqueID PLAY_CASHIER_END = 4170577199U;
+        static const AkUniqueID PLAY_CASHIER_END_RINGUP = 3143524091U;
+        static const AkUniqueID PLAY_CASHIER_MEAT_LOG = 582311066U;
+        static const AkUniqueID PLAY_CASHIER_PLANNINGTOPAY = 2045132466U;
+        static const AkUniqueID PLAY_CASHIER_PLEASESET = 159572298U;
+        static const AkUniqueID PLAY_CASHIER_RANDOM_AFTERNOON = 2053453196U;
+        static const AkUniqueID PLAY_CASHIER_RANDOM_CALLING = 32387144U;
+        static const AkUniqueID PLAY_CASHIER_RANDOM_GDMORNING = 2873879151U;
+        static const AkUniqueID PLAY_CASHIER_SEQUENCE_INSTRUCTIONS = 2258330001U;
+        static const AkUniqueID PLAY_CASHIER_SEQUENCE_JUSTTHEMILK = 2848777120U;
+        static const AkUniqueID PLAY_CASHIER_STRONG_BONES = 3457820653U;
+        static const AkUniqueID PLAY_CASHIER_STRONG_SILENT_TYPE = 589123702U;
+        static const AkUniqueID PLAY_CASHIER_THEREYOUARE = 3298164935U;
+        static const AkUniqueID PLAY_CASHIER_VITIMIN_D = 615435047U;
+        static const AkUniqueID PLAY_FEMALE_VOICE_GOODBYE = 3876234301U;
+        static const AkUniqueID PLAY_FEMALE_VOICE_HAVEANICEDAY = 738920550U;
+        static const AkUniqueID PLAY_FEMALE_VOICE_HELLO = 37077532U;
+        static const AkUniqueID PLAY_MUZAK_GUITAR = 2931435471U;
+        static const AkUniqueID PLAY_PA_MAN_CLOSINGIN10 = 3619978204U;
+        static const AkUniqueID PLAY_PA_MAN_CLOSINGIN30 = 3653533474U;
+        static const AkUniqueID PLAY_PA_MAN_DAIRYINTHEBACK = 4052274365U;
+        static const AkUniqueID PLAY_PA_MAN_HALFOFF = 2102180961U;
+        static const AkUniqueID PLAY_SEQUENCE_WELCOME_WHATCANIDO = 4123174421U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK = 1661994096U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
