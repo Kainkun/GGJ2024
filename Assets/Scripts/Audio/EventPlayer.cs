1f787916cc5eb9898dba1fbda514adf5cfc20ba4
Daniel Walls <walls.dk@gmail.com>
2024-01-28 15:40:54 -0800

    intial audio pass

--- Assets/Scripts/Audio/EventPlayer.cs
+++ Assets/Scripts/Audio/EventPlayer.cs
@@ -4,8 +4,15 @@ namespace ScriptedEvents
 {
     public class EventPlayer : MonoBehaviour
     {
+        public bool autoPlay = false;
         public AK.Wwise.Event eventToPlay;
-        
+
+        private void Start()
+        {
+            if (autoPlay)
+                PlayEvent();
+        }
+
         public void PlayEvent()
         {
             eventToPlay.Post(gameObject);
