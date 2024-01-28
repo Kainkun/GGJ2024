1c5bf4c2dfd9ba86ecdf2f4c741a63282d24b46c
Kain Shepard <kainkun100@gmail.com>
2024-01-28 11:55:27 -0800

    milkdisapear

--- Assets/DisapearingMilk.cs
+++ Assets/DisapearingMilk.cs
@@ -8,4 +8,9 @@ public class DisapearingMilk : MonoBehaviour
     {
         gameObject.SetActive(false);
     }
+
+    public void Poof()
+    {
+        GetComponentInChildren<ParticleSystem>().Play();
+    }
 }
