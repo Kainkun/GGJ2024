92d3eea11eb4dfe92a8448e850443d3780b4fbf0
Daniel Walls <walls.dk@gmail.com>
2024-01-27 18:24:09 -0800

    check paper

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,4 @@
-﻿using Cinemachine;
-using UnityEngine;
+﻿using UnityEngine;
 
 public abstract class PlayerState : State
 {
@@ -18,16 +17,36 @@ public abstract class PlayerState : State
 public class PlayerController : MonoBehaviour
 {
     public PlayerFreeMovementState freeMovementState;
-    public CinemachineVirtualCamera playerCamera;
+    public PlayerCheckPaperState checkPaperState;
+    
+    [Header("References")]
+    [SerializeField] private Transform yawTransform;
+    [SerializeField] private Transform pitchTransform;
+    [SerializeField] private GameObject interactionSource;
+    [SerializeField] private CharacterController characterController;
+    
+    [Header("Settings")]
+    [SerializeField] private PlayerMovementSettings movementSettings;
+    [SerializeField] private PlayerRotationSettings rotationSettings;
+    [SerializeField] private DistanceFootstepSettings footstepSettings;
+    
+    public PlayerMovementLogic MovementLogic;
+    public PlayerInteractionLogic InteractionLogic;
+    public PlayerRotationLogic RotationLogic;
+    public DistanceFootstepLogic FootstepLogic;
 
     public StateMachine<PlayerState> StateMachine { get; private set; }
 
     private void Awake()
     {
+        MovementLogic = new PlayerMovementLogic(movementSettings, new PlayerMovementReferences{yawTransform = yawTransform, characterController = characterController});
+        RotationLogic = new PlayerRotationLogic(rotationSettings, new PlayerRotationReferences{yawTransform = yawTransform, pitchTransform = pitchTransform});
+        InteractionLogic = new PlayerInteractionLogic(new PlayerInteractReferences{interactSource = interactionSource, viewTransform = pitchTransform});
+        FootstepLogic = new DistanceFootstepLogic(footstepSettings, new DistanceFootstepReferences {body = gameObject});
+        
         StateMachine = new StateMachine<PlayerState>();
-        
         freeMovementState.Initialize(this);
-        
+        checkPaperState.Initialize(this);
         StateMachine.TransitionTo(freeMovementState);
     }
 
