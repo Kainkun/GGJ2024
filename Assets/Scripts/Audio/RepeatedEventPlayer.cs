b97c36c05b23197e76dbb3630dda8184f0a93e79
Daniel Walls <walls.dk@gmail.com>
2024-01-28 17:13:03 -0800

    added smalltalk

--- Assets/Scripts/Audio/RepeatedEventPlayer.cs
+++ Assets/Scripts/Audio/RepeatedEventPlayer.cs
@@ -1,13 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class RepeatedEventPlayer : MonoBehaviour
 {
     [SerializeField] private float delaySeconds = 30;
+    [SerializeField] private float initialDelay = 0;
     [SerializeField] private AK.Wwise.Event audioEvent;
+    [SerializeField] private bool autoStart;
 
     private float _elapsed;
     private bool _isPlaying;
-    
+
+    private void Start()
+    {
+        // _elapsed = initialDelay;
+        
+        if (autoStart)
+            Play();
+    }
+
     public void Play()
     {
         _isPlaying = true;
